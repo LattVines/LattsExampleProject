0870667ba7fd297d956d0ad320dd7984696848f0
Latt Vines <LattVines@users.noreply.github.com>
2021-05-13 18:03:58 -0500

    some basic things
    some basic things

---
+++ Assets/FunInterfaces.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class FunInterfaces : MonoBehaviour, IPointerClickHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler, IEndDragHandler, IBeginDragHandler
+{
+
+    //FUN INTERFACES FOR THE UNITY CANVAS OBJECTS ONLY
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("OnPointerClick");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("OnDrag");
+
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        print("OnEndDrag");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        print("OnBeginDrag");
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("OnPinterExit");
+    }
+}
